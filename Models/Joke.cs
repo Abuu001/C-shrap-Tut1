50b3fd2af28856b4df16d61dc2a42d2fcb4264d9
Abuu001 <abulugz@gmail.com>
2021-04-14 21:37:52 +0300

    C# .NET core MVC

---
+++ Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public int JokeAnswer { get; set; }
+        public int JokeQuestion { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
