c92b8a8ccfb53c35c2e58008731a7e7ec1885785
Abuu001 <abulugz@gmail.com>
2021-04-15 21:42:06 +0300

    Add book controller

---
+++ Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JokesWebApp.Models;
+using JokesWebApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JokesWebApp.Controllers
+{
+    public class BookController : Controller
+    {
+
+        public readonly BookRepository _bookRepository =null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View();
+        }
+
+        public BookModel GetBook(int id )
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBook(string bookName ,string authorName)
+        {
+            return _bookRepository.SearchBook(bookName,authorName);
+        }
+    }
+}
