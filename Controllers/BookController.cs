775d9c298a54c2fea744f882a115833c28feb10c
Abuu001 <abulugz@gmail.com>
2021-04-19 21:45:25 +0300

    Add add book route

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -13,42 +13,58 @@ namespace JokesWebApp.Controllers
 
         public readonly BookRepository _bookRepository =null;
 
-        public BookController()
+        public BookController(BookRepository context)
         {
-            _bookRepository = new BookRepository();
+            _bookRepository = context;
+           // _bookRepository = new BookRepository();
         }
         public IActionResult Index()
         {
             return View();
         }
 
-        public ViewResult GetAllBooks()
+        public async Task<ViewResult>  GetAllBooks()
         {
-            var data = _bookRepository.GetAllBooks();
+            var data = await _bookRepository.GetAllBooks();
             return View(data);
         }
 
-        public ViewResult GetBook(int id )
+        public async Task<ViewResult> GetBook(int id )
         {
             //var data= _bookRepository.GetBookById(id);
             //return View(data);
             // we can use this method to pass different data from different sources
-            dynamic data = new System.Dynamic.ExpandoObject();
+            dynamic data =  new System.Dynamic.ExpandoObject();
             data.book = _bookRepository.GetBookById(id);
             data.Name = "Abluu";
             return View(data);
         }
 
-        public ViewResult AddNewBook()
+        public ViewResult AddNewBook(bool isSuccess=false,int bookId =0)
         {
+            ViewBag.IsSuccess = isSuccess;
+            ViewBag.BookId = bookId;
             return View();
         }
 
         [HttpPost]
-        public ViewResult AddNewBook(BookModel bookModel)
+        public async Task<IActionResult>  AddNewBook(BookModel bookModel)
         {
+            if (ModelState.IsValid)
+            {
+                int id =await  _bookRepository.AddNewBook(bookModel);
+
+                if (id > 0)
+                {
+                    return RedirectToAction(nameof(AddNewBook), new { isSuccess = true, bookId = id });
+                }
+            }
+            ViewBag.IsSuccess = false;
+            ViewBag.BookId = 0;
+
             return View();
         }
+
         public List<BookModel> SearchBook(string bookName ,string authorName)
         {
             return _bookRepository.SearchBook(bookName,authorName);
