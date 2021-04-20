33135d766dca44bad3bfdce7a04cdea09910dee1
Abuu001 <abulugz@gmail.com>
2021-04-20 21:25:23 +0300

    Add cover file property

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace JokesWebApp.Models
 {
     public class BookModel
     {
+        [DataType(DataType.DateTime)]
+        [Display(Name ="Choose date and time")]
+        public string MyField { get; set; }
         public int Id { get; set; }
+        [StringLength(100,MinimumLength =5)]
+        [Required(ErrorMessage="Please Enter the title for your book")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Please Enter the Author for your book")]
         public string Author { get; set; }
+        [StringLength(500,MinimumLength =30)]
         public string Description { get; set; }
         public string Category { get; set; }
-        public string Language { get; set; }
-        public int TotalPages { get; set; }
+        public int LanguageId { get; set; }
+        [Required(ErrorMessage = "Please Enter Total pages")]
+        public int? TotalPages { get; set; }
+
+        [Display(Name ="Choose the cover photo of your file")]
+        public IFormFile CoverPhoto { get; set; }
     }
 }
