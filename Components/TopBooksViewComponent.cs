061b5ecafb722644e691d2ea28beb6d8a561ae3a
Abuu001 <abulugz@gmail.com>
2021-04-21 21:51:51 +0300

    Components view

---
+++ Components/TopBooksViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using JokesWebApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Components
+{
+    public class TopBooksViewComponent :ViewComponent
+    {
+        public readonly BookRepository _bookRepository;
+        public TopBooksViewComponent(BookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var books = await _bookRepository.GetTopBooksAsync();
+            return View(books);
+        }
+    }
+}
