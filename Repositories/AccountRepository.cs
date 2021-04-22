3bfda2382ab82428834290b3b8f7d338a3408ce7
Abuu001 <abulugz@gmail.com>
2021-04-22 21:23:15 +0300

    Added signup func

---
+++ Repositories/AccountRepository.cs
@@ -0,0 +1,29 @@
+﻿using JokesWebApp.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AccountRepository(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> CreateUserAsync(SignUpUserModel userModel)
+        {
+            var user = new IdentityUser()
+            {
+                Email =userModel.Email,
+                UserName = userModel.Email
+            };
+           var result = await _userManager.CreateAsync(user,userModel.Password);
+            return result;
+        }
+    }
+}
