a381cb63a7571e8c143a643fb0c8400422fe7c2e
Abuu001 <abulugz@gmail.com>
2021-04-23 19:00:40 +0300

    Add signin interface

--- Repositories/IAccountRepository.cs
+++ Repositories/IAccountRepository.cs
@@ -7,5 +7,7 @@ namespace JokesWebApp.Repositories
     public interface IAccountRepository
     {
         Task<IdentityResult> CreateUserAsync(SignUpUserModel userModel);
+        Task<SignInResult> PasswordSignInAsync(SignInModel signInModel);
+        Task SignoutAsync();
     }
 }
